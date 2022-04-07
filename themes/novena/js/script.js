e8411bd016fcf34e6882082fc7f6e9230170198b
morocco96 <moroccotheboy@gmail.com>
2022-04-07 18:02:41 +0100

    new update

--- themes/novena/js/script.js
+++ themes/novena/js/script.js
@@ -150,6 +150,47 @@
 	});
 
 
+	$('.banner-scroll').slick({
+		infinite: true,
+		arrows: false,
+		autoplay: true,
+		slidesToShow: 6,
+		slidesToScroll: 6,
+		autoplaySpeed: 6000,
+		responsive: [
+		    {
+		      breakpoint: 1024,
+		      settings: {
+		        slidesToShow:6,
+		        slidesToScroll: 6,
+		        infinite: true,
+		        dots: true
+		      }
+		    },
+		    {
+		      breakpoint: 900,
+		      settings: {
+		        slidesToShow:4,
+		        slidesToScroll: 4
+		      }
+		    },{
+		      breakpoint: 600,
+		      settings: {
+		        slidesToShow: 4,
+		        slidesToScroll: 4
+		      }
+		    },
+		    {
+		      breakpoint: 480,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 2
+		      }
+		    }
+		  
+  		]
+	});
+
 
 	var map;
 
